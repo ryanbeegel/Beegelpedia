e4cfd3f9e9824a90af52fe1859e11a692d68fe8d
vagrant <vagrant@rails-dev-box>
2014-07-16 19:34:06 +0000

    Configure app for Produciton emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Beegelpedia::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://beegelpedia.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'beegelpedia.herokuapp.com' }
 end
